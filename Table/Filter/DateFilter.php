c668bf522c54d97e671db05388aca35b0603d070
Julien <git@julienalbinet.fr>
2020-11-14 13:13:51 +0100

    Add DateTime filter

--- Table/Filter/DateFilter.php
+++ Table/Filter/DateFilter.php
@@ -16,64 +16,64 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Filter for date columns.
- * 
+ *
  * @author	Jan Mühlig
  * @since	1.0
  */
 class DateFilter extends AbstractFilter
 {
-	public function needsFormEnviroment()
-	{
-		return true;
-	}
-	
-	protected function setDefaultFilterOptions(OptionsResolver $optionsResolver)
-	{
-		parent::setDefaultFilterOptions($optionsResolver);
-		
-		$optionsResolver->setDefaults(array(
-			'format' => 'Y-m-d',
-			'widget' => 'text',
-			'type' => 'text'
-			//'years' => range( date('Y', strtotime('-5 years')), date('Y', strtotime('+5 years')) ),
-			//'days' => range(1,31)
-		));
-		
-		$optionsResolver->setAllowedValues('widget', array('text'));
-		$optionsResolver->setAllowedValues('type', array('text', 'date'));
-	}
+  public function needsFormEnviroment()
+  {
+    return true;
+  }
 
-	public function getWidgetBlockName() 
-	{
-		if($this->widget === 'text')
-		{
-			return 'date_text_widget';
-		}
-		
-		TableException::filterWidgetNotFound($this->container->get('gus.table_context')->getCurrentTableName(), $this->widget);
-	}
-	
-	public function setValue(array $value)
-	{
-		$dateAsString = $value[$this->getName()];
-		if($dateAsString !== null && $dateAsString !== "")
-		{
-			$timestampFromString = strtotime($dateAsString);
-			
-			$date = new \DateTime();
-			$date->setTimestamp($timestampFromString);
-			$date->setTime(0, 0, 0);
-			
-			$this->value = $date;
-		}
-		else
-		{
-			$this->value = null;
-		}
-	}
-	
-	public function isActive()
-	{
-		return isset($this->value);
-	}
+  protected function setDefaultFilterOptions(OptionsResolver $optionsResolver)
+  {
+    parent::setDefaultFilterOptions($optionsResolver);
+
+    $optionsResolver->setDefaults(array(
+      'format' => 'Y-m-d',
+      'widget' => 'text',
+      'type' => 'text'
+      //'years' => range( date('Y', strtotime('-5 years')), date('Y', strtotime('+5 years')) ),
+      //'days' => range(1,31)
+    ));
+
+    $optionsResolver->setAllowedValues('widget', array('text'));
+    $optionsResolver->setAllowedValues('type', array('text', 'date', 'datetime'));
+  }
+
+  public function getWidgetBlockName()
+  {
+    if($this->widget === 'text')
+    {
+      return 'date_text_widget';
+    }
+
+    TableException::filterWidgetNotFound($this->container->get('gus.table_context')->getCurrentTableName(), $this->widget);
+  }
+
+  public function setValue(array $value)
+  {
+    $dateAsString = $value[$this->getName()];
+    if($dateAsString !== null && $dateAsString !== "")
+    {
+      $timestampFromString = strtotime($dateAsString);
+
+      $date = new \DateTime();
+      $date->setTimestamp($timestampFromString);
+      //$date->setTime(0, 0, 0);
+
+      $this->value = $date;
+    }
+    else
+    {
+      $this->value = null;
+    }
+  }
+
+  public function isActive()
+  {
+    return isset($this->value);
+  }
 }
